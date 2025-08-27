cc0fcae8c9f94e2d54402f1bc65319c0aa66b944
My Name <ashleyz10969@gmail.com>
2025-08-27 23:57:41 +0800

    Fix GitHub Pages deployment issues: set base path to relative and fix resource paths

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './',
   plugins: [
     vue(),
     viteCompression({
